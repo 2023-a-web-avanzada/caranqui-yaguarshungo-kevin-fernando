58328e09e501e295384afad3c7718726d122ab99
KFernandoCaranquiY <kevin.caranqui@epn.edu.ec>
2023-06-11 19:03:15 -0500

    Hasta Funciones js

---
+++ 01-javascript/04-funciones.js
@@ -0,0 +1,23 @@
+function soloLetras(a, b, c){
+    console.log(a,b,c)
+}
+
+function funcionConNombre(){
+
+}
+
+const funcionSinNombre = function(){};
+var funcionSinNombre2 = function(){};
+let funcionSinNombre3 = function(){};
+
+
+///FAT  ARROW > Anonymous
+const functionFatArrow1 = () => {};
+let functionFatArrow2 = () => {};
+var functionFatArrow3 = () => {};
+
+//parametros infinitos
+function sumarNumeros( ...todosLosNumeros){
+}
+
+//funciones asincronas
