644162c68da93f6e8013d9c7e7b68c723f13c845
KFernandoCaranquiY <kevin.caranqui@epn.edu.ec>
2023-07-13 21:06:56 -0500

    Hasta Funciones en JS

--- 01-javascript/04-funciones.js
+++ 01-javascript/04-funciones.js
@@ -2,7 +2,7 @@
 function soloNumeros(a, b, c){
     return a - b +c;
 }
-function soloLetras(a, b, c){
+function soloLetras(a, b, c){ //sinm return, es decir, no devuelve nada
     console.log(a,b,c)
 }
 
@@ -21,8 +21,22 @@ const functionFatArrow1 = () => {};
 let functionFatArrow2 = () => {};
 var functionFatArrow3 = () => {};
 
+const functionFactArraw5 = (parametro) =>{
+    return parametro +1;
+}
+const functionfactArrow6 = (parametro) => parametro +1; //Una sola linea, omitió return, omiiton llaves
+const funcionFatArrow7 = parametro => parametro +1; //solo tenemos un parametro y omitimos el parentesis
+
 //parametros infinitos
 function sumarNumeros( ...todosLosNumeros){
+    let total = 0;
+    todosLosNumeros.forEach(
+        (valorActual)=>{
+            total =  total + valorActual;
+        }
+    );
+    return total;
+    //return todosNumeros.reduce((a,v) => a +v, 0);
 }
 
 //funciones asincronas
